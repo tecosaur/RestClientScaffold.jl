48d78ff85d5efb0a9da390928a2e970bced28c80
TEC <git@tecosaur.net>
2024-12-11 03:38:36 +0800

    Preliminary XML support

--- src/utilities.jl
+++ src/utilities.jl
@@ -42,6 +42,9 @@ This macro conveniently combines the following pieces:
   `StructTypes.names` (if needed) to define the JSON field mapping.
 - `RestClient.dataformat` to declare that this struct is JSON-formatted.
 
+Note the `name."json_field"` syntax demonstrated in the examples, that allows
+for declaration of the JSON object key that should be mapped to the field.
+
 Optionally the JSON representation `kind` can be specified. It
 defaults to `Struct`, but can any of: `Struct`, `Dict`, `Array`,
 `Vector`, `String`, `Number`, `Bool`, `Nothing`.
@@ -69,3 +72,46 @@ macro jsondef(arg::Any)
     end
 end
 
+"""
+    @xmldef struct ... end
+
+Define a struct that can be used with `XML`.
+
+This macro conveniently combines the following pieces:
+- XML deserialization
+- `@kwdef` to define keyword constructors for the struct.
+- `RestClient.dataformat` to declare that this struct is XML-formatted.
+
+Note the `name."xpath"` syntax demonstrated in the examples, that allows for
+declaration of the (simple) XPath that should be used to extract the field. The
+subset of supported XPath components are:
+- `nodetag` to extract all immediate children with a given tag name
+- `relative/node/paths`
+- `*` to extract all children
+- `text()` to extract the text content of a node
+- `@attr` to extract the value of an attribute
+- `nodetag[i]` to extract the `i`-th child of type `nodetag`
+- `nodetag[last()]` to extract the last child of type `nodetag`
+
+!!! warning "Soft XML dependency"
+    This macro is implemented in a package extension, and so
+    requires `XML` to be loaded before it can be used.
+
+# Examples
+
+```julia
+@xmldef struct DocumentStatus
+    exists."status/@exists"::Bool
+    status."status/text()"::String
+    url."status/@url"::Union{String, Nothing} = nothing
+    age."status/@age"::Int
+end
+```
+"""
+macro xmldef(arg::Any)
+    if arg isa Expr
+        throw(ArgumentError("@xmldef requires XML to be loaded"))
+    else
+        throw(ArgumentError("@xmldef expects a struct definition"))
+    end
+end
